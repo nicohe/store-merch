54ffbc99837ab5225199e7f0cf0f5e39547ba104
nicohe <h.nico@hotmail.com.ar>
2021-02-23 16:44:02 -0300

    Update useGoogleAddress.js

--- src/hooks/useGoogleAddress.js
+++ src/hooks/useGoogleAddress.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const useGoogleAddress = address => {
   const [map, setMap] = useState({});
-  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBJgDOkg1gQh1en-M9A5QJrIciA4v9BZ4w`;
+  const API = ``;
 
   useEffect(async () => {
     const response = await axios(API);
